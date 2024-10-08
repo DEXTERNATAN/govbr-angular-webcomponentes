db772fe8ac41ecd953bc45a4f9737551cd7197b3
Natanael Leite <natansl@gmail.com>
2024-10-08 16:06:16 -0300

    feat: adiciona nova estrutura a pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { FormulariosComponent } from './formularios/formularios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'componentes', component: ComponentesComponent },
+  { path: 'formularios', component: FormulariosComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redireciona para o Dashboard por padrão
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ComponentesComponent } from './componentes/componentes.component';
+import { FormulariosComponent } from './formularios/formularios.component';
 
 // Importação do Webcomponents Angular do GovBR
 import { WebcomponentsAngularModule } from '@govbr-ds/webcomponents-angular';
 import '@govbr-ds/core/dist/core.min.css';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ComponentesComponent,
+    FormulariosComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,6 +29,6 @@ import '@govbr-ds/core/dist/core.min.css';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
